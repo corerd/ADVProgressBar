6ea9f88f3a44a4e968c3ce295d7897e14b1ad1bb
corerd <corerd@live.com>
2013-02-17 07:16:16 +0100

    Using the ADVPercentProgressBar View tutorial

--- search/all_73.js
+++ search/all_73.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['showpercent',['showPercent',['../interface_a_d_v_percent_progress_bar.html#afc89fabd5a3f550d8cae564eb2ac4162',1,'ADVPercentProgressBar']]],
-  ['slidervaluechanged_3a',['sliderValueChanged:',['../interface_a_d_v_view_controller.html#a068c959473d719b4e6eeefa4ba00fd8f',1,'ADVViewController']]]
+  ['showpercent',['showPercent',['../interface_a_d_v_percent_progress_bar.html#afc89fabd5a3f550d8cae564eb2ac4162',1,'ADVPercentProgressBar']]]
 ];
